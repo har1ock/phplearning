7d5348f25f0a1f7b5dbaae91be8684ba1c830764
harlock <xarlokua@gmail.com>
2025-09-03 19:19:04 +0300

    initital commit

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    echo "<p>Привіт, $name!</p>";
+}
+?>
+
+<form method = "post">
+    <input type="text" name="name" placeholder="Введіть ім'я">
+    <button type="submit">Відправити</button>
+</form>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "<h1>Привіт, PHP!</h1>";
+
+$name = "Bob";
+echo "<p>Привіт, $name!</p>";
+
+// simple calculator
+$a = 10;
+$b = 5;
+echo '<p>Sum: ' . ($a + $b) . '</p>';
+echo "<p>Difference: " . ($a - $b) . "</p>";
+echo "<p>Multiply:" . ($a * $b) . "</p>";
+echo "<p>Division:" . ($a / $b) . "</p>";
+?>
