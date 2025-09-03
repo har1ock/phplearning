fe5573fd42eea22259897923e1033cc9316460b5
harlock <xarlokua@gmail.com>
2025-09-03 21:29:39 +0300

    ex 1 comp;

--- phpsecondday/varandtypes.php
+++ phpsecondday/varandtypes.php
@@ -1,4 +1,5 @@
 <?php
+# Ex 1.1
 $name = "Bob";
 $age = 20;
 $height = 1.80;
@@ -12,5 +13,18 @@ if ($isWorking == True) {
 } else {
     echo "Безробітній";
 }
-    
+
+# Ex 1.2
+$a = 10;
+$b = 8;
+echo "<p>Sum = " . ($a + $b) . "</p>";
+echo "<p>Average = ". (($a + $b)/2) . "</p>";
+
+# Ex 1.3
+$number = "123";
+$number = (int)($number);
+$number = $number + 10;
+echo "<p>Number is ". ($number) . "</p>";
+
+
 ?>
