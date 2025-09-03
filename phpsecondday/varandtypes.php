8f7456e062bb387e64ec42dfb8cead6bdd3b4fa8
harlock <xarlokua@gmail.com>
2025-09-03 21:04:41 +0300

    ex 1.1

---
+++ phpsecondday/varandtypes.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Bob";
+$age = 20;
+$height = 1.80;
+$isWorking = True;
+
+echo "<p> Name: " . $name . "</p>";
+echo "<p> Age: " . $age . "</p>";
+echo "<p> Height: " . $height . "</p>";
+if ($isWorking == True) {
+    echo "Працює";
+} else {
+    echo "Безробітній";
+}
+    
+?>
