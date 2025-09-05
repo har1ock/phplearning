754fe3dd932fb7e1708e2bd567e1287ca034ec48
harlock <xarlokua@gmail.com>
2025-09-05 22:43:52 +0300

    ex1 classes before update

--- phpfourthday/Account.php
+++ phpfourthday/Account.php
@@ -5,30 +5,34 @@ class Account{
     private string $owner;
     private float $balance = 0;
 
-    public function __construct(){
-
+    public function __construct(string $owner, float $balance){
+        $this->setOwner($owner);
+        $this->setBalance($balance);
     }
     
     public function setOwner(string $owner): void {
-
+        $this->owner = trim($owner);
     }
     
     public function setBalance(float $balance): void {
         if($balance < self::MIN_BALANCE){
-
+            throw new InvalidArgumentException(
+            "Баланс не може бути менше - ". self::MIN_BALANCE);
         }
+        $this->balance = $balance;
     }
-    public function deposit(): float {
-
+    public function deposit(float $sum): float {
+        $this->balance = $this->balance + $sum;
+        return $this->balance;
     }
-
-    
-    public function withdraw(): float { 
-
+  
+    public function withdraw(float $sum): float { 
+        $this->balance = $this->balance - $sum;
+        return $this->balance;
     }
     
     public function getBalance(): float {
-        return
+        return $this->balance;
     }
 }
 ?>
--- phpfourthday/index.php
+++ phpfourthday/index.php
@@ -7,7 +7,11 @@
 </head>
 <body>
     <?php
-
+    require_once 'Account.php';
+    $acc1 = new Account('George', 200);
+    $acc1 ->deposit(500);
+    $acc1 ->withdraw(300);
+    echo $acc1->getBalance();
     ?>
 </body>
 </html>
